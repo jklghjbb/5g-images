5565ac1ddc0b4f431d5d7e9d9d959cd617d00c98
Hongjie Xie <2632910583@qq.com>
2024-10-01 15:01:20 +0200

    choose the main branch

--- images/srsran-5g/build.sh
+++ images/srsran-5g/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define variables
-VERSION="26860609f9be3443f151334cbf235fb7a8abcd88"  # Replace with the desired version of srsRAN
+VERSION="ee1d86cd80841f47061bbdbe25786e3702059937"  # Replace with the desired version of srsRAN
 IMAGE_NAME="srsran_builder"
 export BUILDKIT_PROGRESS=plain
- ]
+
 # Build the Docker image
 echo "Building the Docker image with srsRAN version $VERSION..."
 docker build --build-arg version=$VERSION --build-arg num_procs=15 -t $IMAGE_NAME .
